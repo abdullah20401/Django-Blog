d58f81f91d269398adcf3687119c18dc25a1478e
Abdullah Siddiqui <siddiquiabdullah2017@gmail.com>
2020-11-13 05:43:26 +0000

    Video Part 5

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Abdullah. S',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': "November, 11 2020"
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': "December, 31 2020"
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
